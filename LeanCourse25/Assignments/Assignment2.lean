402d7d906ee60967d19a92266e019e4be22cafe7
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-10-23 14:02:10 +0200

    Add Assignment 2

---
+++ LeanCourse25/Assignments/Assignment2.lean
@@ -0,0 +1,254 @@
+import Mathlib.Analysis.Complex.Exponential
+open Real Function Set Nat
+
+/-
+
+* Hand in the solutions to the exercises below. Deadline: 30.10.2025 at 10:00.
+
+* Make sure the file you hand-in compiles without error.
+  Use `sorry` if you get stuck on an exercise.
+-/
+
+
+/-! # Exercises to practice. -/
+
+example (p q r s : Prop) (h : p ∧ q → r) (hp : p) (h' : q → s) : q → r ∧ s := by
+  sorry
+  done
+
+example {α : Type*} {p q : α → Prop} (h : ∀ x, p x → q x) :
+    (∃ x, p x) → (∃ x, q x) := by
+  sorry
+  done
+
+-- Exercise: prove this by contraposition.
+example : 2 ≠ 4 → 1 ≠ 2 := by
+  sorry
+  done
+
+/- Prove the following with basic tactics,
+in particular without using `tauto`, `grind` or lemmas from Mathlib. -/
+example {α : Type*} {p : α → Prop} {r : Prop} :
+    ((∃ x, p x) → r) ↔ (∀ x, p x → r) := by
+  sorry
+  done
+
+/- Prove the following with basic tactics,
+in particular without using `tauto`, `grind` or lemmas from Mathlib. -/
+example {α : Type*} {p : α → Prop} {r : Prop} :
+    (∃ x, p x ∧ r) ↔ ((∃ x, p x) ∧ r) := by
+  sorry
+  done
+
+/- Prove the following without using `push_neg` or lemmas from Mathlib.
+You will need to use `by_contra` in the proof. -/
+example {α : Type*} (p : α → Prop) : (∃ x, p x) ↔ (¬ ∀ x, ¬ p x) := by
+  sorry
+  done
+
+def SequentialLimit (u : ℕ → ℝ) (l : ℝ) : Prop :=
+  ∀ ε > 0, ∃ N, ∀ n ≥ N, |u n - l| < ε
+
+example (a : ℝ) : SequentialLimit (fun n : ℕ ↦ a) a := by
+  sorry
+  done
+
+/-
+`(n)!` denotes the factorial function on the natural numbers.
+The parentheses are mandatory when writing.
+Use `calc` to prove this.
+You can use `exact?` to find lemmas from the library,
+such as the fact that factorial is monotone. -/
+example (n m k : ℕ) (h : n ≤ m) : (n)! ∣ (m + 1)! := by
+  sorry
+  done
+
+example (a b c x y : ℝ) (h : a ≤ b) (h2 : b < c) (h3 : x ≤ y) :
+    a + exp x ≤ c + exp (y + 2) := by
+  sorry
+  done
+
+-- Use `rw?` or `rw??` to help you in the following calculation.
+-- Alternatively, write out a calc block by hand.
+example {G : Type*} [Group G] {a b c d : G}
+    (h : a⁻¹ * b * c * c⁻¹ * a * b⁻¹ * a * a⁻¹ = b) (h' : b * c = c * b) : b = 1 := by
+  sorry
+
+/-- Prove the following using `linarith`.
+Note that `linarith` cannot deal with implication or if and only if statements. -/
+example (a b c : ℝ) : a + b ≤ c ↔ a ≤ c - b := by
+  sorry
+  done
+
+
+/- You can prove many equalities and inequalities by being smart with calculations.
+In this case `linarith` can also prove this, but `calc` can be a lot more flexible. -/
+example {x y : ℝ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 := by
+  calc
+    y = y + 2 * x - 2 * x := by ring
+    _ ≥ 3 - 2 * x := by gcongr
+    _ = 9 - 2 * (x + 3) := by ring
+    _ ≥ 9 - 2 * 2 := by gcongr
+    _ > 3 := by norm_num
+
+/-- It can be useful to add a `+ 0` in a calculational proof for `gcongr` -/
+example {m n : ℤ} : n ≤ n + m ^ 2 := by
+  -- gcongr doesn't make progress here
+  calc
+    n = n + 0 := by ring
+    _ ≤ n + m ^ 2 := by gcongr; exact sq_nonneg m
+
+/- Sometimes `congr`/`gcongr` goes too deep into a term.
+In that case, you can give `gcongr` a pattern with how deep it should enter the terms.
+When the pattern contains `?_`, it creates a subgoal with the corresponding terms
+on each side of the inequality.
+For `congr` you can also do this using the tactic `congrm`. -/
+example {a₁ a₂ b₁ b₂ c₁ c₂ : ℝ} (hab : a₁ + a₂ = b₁ + b₂) (hbc : b₁ + b₂ ≤ c₁ + c₂) :
+    a₁ + a₂ + 1 ≤ c₁ + c₂ + 1 := by
+  calc a₁ + a₂ + 1 = b₁ + b₂ + 1 := by congrm ?_ + 1; exact hab
+    _ ≤ c₁ + c₂ + 1 := by gcongr ?_ + 1 -- gcongr automatically applies `hbc`.
+
+
+example {m n : ℤ} : n - m ^ 2 ≤ n + 3 := by
+  sorry
+  done
+
+example {a : ℝ} (h : ∀ b : ℝ, a ≥ -3 + 4 * b - b ^ 2) : a ≥ 1 := by
+  sorry
+  done
+
+
+
+/-! # Exercises to hand-in. -/
+
+section Logic
+-- Prove the following statements with basic tactics,
+-- in particular without using `tauto`, `grind` or lemmas from mathlib.
+
+/-- The function`f : ℝ → ℝ` is continuous at `x₀`.-/
+def ContinuousAtPoint (f : ℝ → ℝ) (x₀ : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x, |x - x₀| < δ → |f x - f x₀| < ε
+
+def Continuous' (f : ℝ → ℝ) := ∀ x, ContinuousAtPoint f x
+
+-- Exercise for you. Remember that you can use `unfold` to expand a definition.
+example (f g : ℝ → ℝ) (hfg : ∀ x, ContinuousAtPoint f x ↔ ContinuousAtPoint g x) :
+    Continuous' f ↔ Continuous' g := by
+  sorry
+  done
+
+def All (p : ℝ → Prop) := ∀ x, p x
+
+example (p q : ℝ → Prop) (h : ∀ x, p x ↔ q x) :
+    All p ↔ All q := by
+  unfold All
+  simp_rw [h]
+
+example (p q : ℝ → Prop) (h : ∀ x, p x ↔ q x) :
+    (∃ x, p x) ↔ (∃ x, q x) := by
+  simp_rw [h]
+
+-- Is the following true? If yes, prove it in Lean.
+-- If not, give a counterexample and prove it. (What do you have to do to do so?)
+example (p q : ℕ → Prop) (h: (∃ x, p x) ↔ (∃ x, q x)) : ∀ x, p x ↔ q x := by
+  sorry
+
+/- Prove the following with basic tactics, without using `tauto` or lemmas from Mathlib. -/
+lemma exists_distributes_over_or {α : Type*} {p q : α → Prop} :
+    (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := by
+  sorry
+  done
+
+end Logic
+
+section Functions
+
+-- Let us investigate the function example from the lecture further.
+
+-- This is how to say "a natural number p is prime" in mathlib.
+#check Nat.Prime
+
+-- The following theorem is the key ingredient to it.
+-- (You have not seen the syntax `[hp: Fact (Nat.Prime p)]` yet: this is related to implicit arguments.
+--  You can assume it says `(hp: Nat.Prime p)`. We will discuss the precise difference later.)
+--
+-- Use `exact?`, `apply?` or `rw??` to find this theorem in mathlib.
+-- Describe what you are doing.
+example (p : ℕ) [hp: Fact (Nat.Prime p)] (x : ZMod p) : x ^ p = x := by
+  sorry
+  done
+
+-- The above theorem has a name. What is it?
+-- Use this name to find the same result using leansearch.net.
+-- Describe every step you're performing.
+
+-- Prove the following.
+example (p : ℕ) [Fact (Nat.Prime p)] :
+    (fun x ↦ x ^ 7 + 2 * x ^ (2 * p - 1) : ZMod p → ZMod p) = (fun x ↦ 3 * x) := by
+  sorry
+  done
+
+-- Prove the following.
+example (p : ℕ) [Fact (Nat.Prime p)] {a b c d : ZMod p} :
+    (fun x ↦ a * x ^ 7 + b * x ^ (2 * p - 1) + c * x ^ (3 * p - 2) + d * x ^ (5 * p - 4)) =
+      (fun x ↦ (a + b + c + d) * x) := by
+  sorry
+  done
+
+end Functions
+
+
+/- Prove that the sum of two converging sequences converges. -/
+lemma sequentialLimit_add {s t : ℕ → ℝ} {a b : ℝ}
+      (hs : SequentialLimit s a) (ht : SequentialLimit t b) :
+    SequentialLimit (fun n ↦ s n + t n) (a + b) := by
+  sorry
+  done
+
+/- It may be useful to case split on whether `c = 0` is true. -/
+lemma sequentialLimit_mul_const {s : ℕ → ℝ} {a : ℝ} (c : ℝ) (hs : SequentialLimit s a) :
+    SequentialLimit (fun n ↦ c * s n) (c * a) := by
+  sorry
+  done
+
+/-- Prove this using a calculation. -/
+lemma exercise_square {m n k : ℤ} (h : m ^ 2 + n ≤ 2) : n + 1 ≤ 3 + k ^ 2 := by
+  sorry
+  done
+
+
+section Growth
+
+variable {s t : ℕ → ℕ} {k : ℕ}
+
+/- `simp` can help you simplify expressions like the following. -/
+example : (fun n ↦ n * s n) k = k * s k := by simp
+example (a b c : ℕ) : c ≥ max a b ↔ c ≥ a ∧ c ≥ b := by simp
+
+/- Given two sequences of natural numbers `s` and `t`.
+We say that `s` eventually grows faster than `t` if for all `k : ℕ`,
+`s_n` will be at least as large as `k * t_n` for large enough `n`. -/
+def EventuallyGrowsFaster (s t : ℕ → ℕ) : Prop :=
+  ∀ k : ℕ, ∃ N : ℕ, ∀ n ≥ N, k * t n ≤ s n
+
+/- show that `n * s n` grows (eventually) faster than `s n`. -/
+lemma eventuallyGrowsFaster_mul_left :
+    EventuallyGrowsFaster (fun n ↦ n * s n) s := by
+  sorry
+  done
+
+/- Show that if `sᵢ` grows eventually faster than `tᵢ` then
+`s₁ + s₂` grows eventually faster than `t₁ + t₂`. -/
+lemma eventuallyGrowsFaster_add {s₁ s₂ t₁ t₂ : ℕ → ℕ}
+    (h₁ : EventuallyGrowsFaster s₁ t₁) (h₂ : EventuallyGrowsFaster s₂ t₂) :
+    EventuallyGrowsFaster (s₁ + s₂) (t₁ + t₂) := by
+  sorry
+  done
+
+/- Find a positive function that grows faster than itself when shifted by one. -/
+lemma eventuallyGrowsFaster_shift : ∃ (s : ℕ → ℕ),
+    EventuallyGrowsFaster (fun n ↦ s (n+1)) s ∧ ∀ n, s n ≠ 0 := by
+  sorry
+  done
+
+end Growth
